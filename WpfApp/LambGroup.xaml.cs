bdd4d09c98ffe60aa6ed8d7d6379487ce97f5593
insoftpro <insoftpro@DESKTOP-HCOS156>
2022-05-17 15:53:17 +0300

    Add project files.

---
+++ WpfApp/LambGroup.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp
+{
+    /// <summary>
+    /// Interaction logic for LambGroup.xaml
+    /// </summary>
+    public partial class LambGroup : Page
+    {
+        public LambGroup()
+        {
+            InitializeComponent();
+        }
+
+        private void Lamb_State(object sender, RoutedEventArgs e)
+        {
+            var lambState = LambImg.Source;
+
+            var LambOff = "pack://application:,,,/off.png";
+            var LambSrc = Convert.ToString(LambImg.Source);
+            if (LambSrc == LambOff)
+            {
+            LambImg.Source = new BitmapImage(new Uri(@"/on.png", UriKind.Relative));
+
+            }
+            else
+            {
+                LambImg.Source = new BitmapImage(new Uri(@"/off.png", UriKind.Relative));
+            }
+
+        }
+    }
+}
---
+++ WpfApp/TextGroup.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp
+{
+    /// <summary>
+    /// Interaction logic for TextGroup.xaml
+    /// </summary>
+    public partial class TextGroup : Page
+    {
+        public TextGroup()
+        {
+            InitializeComponent();
+        }
+
+        private void Random_Num(object sender, RoutedEventArgs e)
+        {
+            Random slumpGenerator = new Random();
+
+            int rndmNum = slumpGenerator.Next(0, 1000);
+
+            TextBox.Text += rndmNum.ToString();
+        }
+    }
+}
